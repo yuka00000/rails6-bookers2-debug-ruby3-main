9c061b56a05b7c4cefa0cfcecf66a400c113d219
EC2 Default User <ec2-user@ip-172-31-5-36.ap-northeast-1.compute.internal>
2023-09-07 10:23:05 +0000

    [Finish]応用課題7d

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -66,6 +66,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :user_id)
+    params.require(:book).permit(:title, :body, :user_id, :star)
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,10 @@ import "../stylesheets/application";
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
